e9fececa71d099bbe97049da8faf6ea673b2b381
glerique <gael.lerique@gmail.com>
2020-01-30 15:19:06 +0100

    Create header

---
+++ includes/header.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" href="assets/img/favicon.png" type="image/png">
+    <title>Blog PHP Gaël Lerique</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/themify-icons.css">
+    <link rel="stylesheet" href="assets/vendors/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="assets/vendors/owl-carousel/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/vendors/animate-css/animate.css">
+    <!-- main css -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
+</head>
+<body>
+
+    <!--================Header Menu Area =================-->
+    <header class="header_area">	
+        <div class="main_menu">
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <div class="container">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <a class="navbar-brand logo_h" href="index.html"><img src="assets/img/logo.png" alt=""></a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+                        <ul class="nav navbar-nav menu_nav ml-auto">
+                            <li class="nav-item"><a class="nav-link" href="">Accueil</a></li> 
+                            <li class="nav-item"><a class="nav-link" href="">CV</a></li> 
+                            <li class="nav-item submenu dropdown">
+                                <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Administration</a>
+                                <ul class="dropdown-menu">
+                                    <li class="nav-item"><a class="nav-link" href="">Ajouter</a></li> 
+                                    <li class="nav-item"><a class="nav-link" href="">Liste</a></li>                                    
+                                </ul>
+                            </li>                            
+                            <li class="nav-item"><a class="nav-link" href="">Contact</a></li>
+                        </ul>
+                    </div>
+                    <div class="right-button">
+                        <ul>                            
+                            <li><a class="sign_up" href="">Inscription</a></li>
+                        </ul>
+                    </div> 
+                </div>
+            </nav>
+        </div>
+    </header>
+     <!--================Home Banner Area =================-->
+     <section class="home_banner_area">
+        <div class="banner_inner d-flex align-items-center">
+            <div class="overlay"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 offset-lg-6 col-xl-5 offset-xl-7">
+                        <div class="banner_content">
+                            <h3>Gaël Lerique<br/>Développeur PHP</h3>
+                            <p>Un travail acharné vient à bout de tout.</p>
+                         </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
