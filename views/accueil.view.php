e2628a7ea2a1da31831966fca07642a658116ae1
glerique <gael.lerique@gmail.com>
2020-01-30 15:15:36 +0100

    Create accueil view with list of post

---
+++ views/accueil.view.php
@@ -0,0 +1,42 @@
+ <!--================ Start Blog Area =================-->
+ <section class="latest-blog-area area-padding">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-12">
+                    <div class="area-heading">
+                        <h4>Derniers Articles</h4>
+                        <p>Life firmament under them evening make after called dont saying likeness<br> isn't wherein also forth she'd air two without</p>
+                    </div>           
+                </div>
+            </div>
+
+            <div class="row">
+            <?php 
+      foreach($manager->getListPost() as $value){ 
+        ?>  
+                <div class="col-lg-4 col-md-6 ">
+                <a href = "index.php?action=articles&id=<?= $value->getId(); ?>">
+                    <div class="single-blog">
+                        <div class="thumb">
+                            <img class="img-fluid w-100" src="assets/img/blog/2.png" alt="">
+                        </div>
+                        <div class="single-blog-content">
+                            <p class="tag">Software / Business</p>
+                            <p class="date"><?= $value->getDateAjout(); ?> </p>
+                            <h4>
+                            <?= ucfirst ($value->getTitre()); ?>
+                            </h4>
+                            <p> <?= $value->getChapo(); ?></p>
+                            <div class="meta-bottom d-flex">
+                                <p class="likes"><i class="ti-comments"></i> 02 Comments</p>                                
+                            </div>
+                            </a>
+
+                        </div>
+                    </div>
+                </div>               
+                <?php } ?>            
+              </div>
+        </div>
+    </section>
+    <!--================ End Blog Area =================-->
