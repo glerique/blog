6be17ed6bd4972f4ca796ec362c86ce562291af0
glerique <gael.lerique@gmail.com>
2020-02-27 19:27:58 +0100

    Add namespace models

--- models/Database.php
+++ models/Database.php
@@ -1,12 +1,13 @@
 <?php
+
+namespace models;
+
 class Database{
 
   protected function dbConnect()
       {
-          //$db = new PDO('mysql:host=db;dbname=blog;charset=utf8', 'root', 'test');
-          $db = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
-          $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          $db = new \PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
+          $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
           return $db;
       }
 }
-?>
