71b24f503fa098603f4b5178ef1e688b4d0b06a3
glerique <gael.lerique@gmail.com>
2020-02-27 14:53:42 +0100

    Create view of add a post

---
+++ views/addPost.view.php
@@ -0,0 +1,66 @@
+<section class="hero-banner d-flex align-items-center">
+        <div class="container text-center">
+            <h2>Ajouter</h2>            
+        </div>
+    </section>
+ 
+    <section class="contact-section area-padding">
+    <div class="container">
+              <div class="row">
+        <div class="col-12">
+          <h2 class="contact-title">Ajouter</h2>
+        </div>
+        <div class="col-lg-8">
+          <form class="form-contact contact_form" method="post">
+            <div class="row">           
+              <div class="col-12">
+                <div class="form-group">
+                    Id : <input class="form-control" type="text" name="id">
+                </div>
+              </div>
+              <div class="col-12">
+                <div class="form-group">
+                  Titre : <input class="form-control" type="text" name="titre">
+                </div>
+              </div>
+              <div class="col-12">
+                <div class="form-group">
+                  Chapo : <input class="form-control" type="text" name="chapo">
+                </div>
+              </div>
+              <div class="col-12">
+                <div class="form-group">
+                  Contenu : <input class="form-control" type="text" name="contenu">
+                </div>
+              </div>
+              <div class="col-12">
+                <div class="form-group">
+                  Date ajout : <input class="form-control" type ="date" name="dateAjout">
+                </div>
+              </div>
+              <div class="col-12">
+                <div class="form-group">
+                  Date modification : <input class="form-control" type ="date" name="dateModification">
+                </div>
+              </div>
+              <div class="col-12">
+                <div class="form-group">
+                  Statut : <input class="form-control" type="text" name="statut">
+                </div>
+              </div>
+              <div class="col-12">
+                <div class="form-group">
+                UtilisateurID : <input class="form-control" type="text" name="utilisateurId">
+                </div>
+              </div>
+            </div>
+            <div class="form-group mt-3">
+           <input type="submit" class="button button-contactForm" name="ajouter" value="poster">
+            </div>
+          </form>
+
+
+        </div>       
+      </div>
+    </div>
+</section>
