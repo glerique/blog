6c00fbe58747b2e5f13aa9b4ead7f71c54576597
glerique <gael.lerique@gmail.com>
2020-03-30 14:30:15 +0200

    Refactor controller with static function render

--- controllers/Post.php
+++ controllers/Post.php
@@ -13,41 +13,53 @@ class Post{
     //Affiche la page page d'accueil 
     function accueil(){
         $manager = $this->modelManager;
-        require('views/accueil.view.php'); 
-    }        
+        $posts = $manager->getList();        
+        \models\Renderer::render("accueil", compact('posts'));
+        
+    }
 
-    function afficher(){
-        //Affiche la fiche produit en fonction de l'id recupéré par GET    
+    //Affiche la fiche produit en fonction de l'id recupéré par GET
+    function afficher(){            
         $manager = new $this->modelManager;        
         $post= $manager->get($_GET['id']);
-        require('views/post.view.php');
+        \models\Renderer::render("post", compact('post'));
+       
     }
     
     function ajouterPost(){                
+        $statut = "Attente";
+        $dateAjout = date('Y-m-d');
+        $utilisateurId = $_SESSION['user']['id'];
+        
         $manager = new $this->modelManager;          
         $post = new \models\Post([
                 'titre' => $_POST['titre'], 
                 'chapo' => $_POST['chapo'], 
                 'contenu' => $_POST['contenu'], 
-                'dateAjout' => $_POST['dateAjout'], 
-                'dateModification' => $_POST['dateModification'], 
-                'statut' => $_POST['statut'], 
-                'utilisateurId' =>$_POST['utilisateurId']
+                'dateAjout' => $dateAjout,                               
+                'statut' => $statut
+                
             ]);
      
-     $manager->add($post);     
-     require('views/accueil.view.php');               
+     $manager->add($post);
+     $this->redirectWithSuccess(
+        "index.php?controller=Post&action=liste",
+        "Post ajouté avec succès !"
+    );   
+                   
     }
 
     function modifier(){
+        
         $manager = new $this->modelManager;
         $post = $manager->get($_GET['id']);
-        require('views/updatePost.view.php'); 
-        
+        \models\Renderer::render("updatePost", compact('post'));
+                
     }
 
     function ajouter(){
-        require('views/addPost.view.php');
+       
+        \models\Renderer::render("addPost");
     }
 
     function ModifierPost(){
@@ -55,9 +67,8 @@ class Post{
         $id = $_POST['id'];
         $titre = $_POST['titre'];
         $chapo = $_POST['chapo'];
-        $contenu = $_POST['contenu'];
-        $dateAjout = $_POST['dateAjout'];
-        $dateModification = $_POST['dateModification'];
+        $contenu = $_POST['contenu'];        
+        $dateModification = date('y-m-d');
         $statut = $_POST['statut'];
         $utilisateurId = $_POST['utilisateurId'];
 
@@ -66,8 +77,7 @@ class Post{
         'id' => $id,
         'titre' => $titre,
         'chapo' => $chapo,
-        'contenu' => $contenu,
-        'dateAjout' => $dateAjout,
+        'contenu' => $contenu,        
         'dateModification' => $dateModification,
         'statut' => $statut,
         'utilisateurId' => $utilisateurId  
@@ -75,20 +85,52 @@ class Post{
 
         $manager->update($post);
         $manager = new $this->modelManager;
-        require('views/liste.view.php');
+        $this->redirectWithSuccess(
+            "index.php?controller=Post&action=liste",
+            "Post modifié avec succès !"
+        );
+        
     }
 
     function liste(){
         $manager = new $this->modelManager;
-        require('views/liste.view.php');
+        $posts = $manager->getList();
+        \models\Renderer::render("liste", compact('posts'));
     }
 
     function supprimer(){
         $id = $_GET['id'];
         $manager = new $this->modelManager;       
         $manager->delete($id);               
-        require('views/liste.view.php');
-    }    
-}
+        $this->redirectWithSuccess(
+            "index.php?controller=Post&action=liste",
+            "Post supprimé avec succès !"
+        );
+    }
+    
+    protected function redirectWithError(string $url, string $message)
+    {
+        \models\Session::addFlash('error', $message);
+        \models\Http::redirect($url);
+    }
+    
+    protected function redirectWithSuccess(string $url, string $message)
+    {
+        \models\Session::addFlash('success', $message);
+        \models\Http::redirect($url);
+    }
 
+    protected function redirectBackWithError(string $message)
+    {
+        $url = $_SERVER['HTTP_REFERER'];
+        $this->redirectWithError($url, $message);
+    }
+
+    
+    protected function redirectBackWithSuccess(string $message)
+    {
+        $url = $_SERVER['HTTP_REFERER'];
+        $this->redirectWithSuccess($url, $message);
+    }
+}
 ?>
