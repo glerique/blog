b1a107ad3d1ba0ed246a475e0084c5694b5f3966
glerique <gael.lerique@gmail.com>
2020-03-31 18:23:48 +0200

    Change name of properties

--- views/updatePost.view.php
+++ views/updatePost.view.php
@@ -1,13 +1,13 @@
 <section class="hero-banner d-flex align-items-center">
         <div class="container text-center">
-            <h2>Modifier</h2>            
+            <h2>Modifier un Post</h2>            
         </div>
     </section>
      <section class="contact-section area-padding">
     <div class="container">
               <div class="row">
         <div class="col-12">
-          <h2 class="contact-title">Modifier</h2>
+          <h2 class="contact-title">Modifier un Post</h2>
         </div>
         <div class="col-lg-8">
           <form class="form-contact contact_form" method="post" action="index.php?controller=Post&action=modifierPost">
@@ -19,30 +19,30 @@
               </div>
               <div class="col-12">
                 <div class="form-group">
-                  Titre : <input class="form-control" type="text" name="titre" value="<?= $post->getTitre(); ?>">
+                  Titre : <input class="form-control" type="text" name="title" value="<?= $post->getTitle(); ?>">
                 </div>
               </div>
               <div class="col-12">
                 <div class="form-group">
-                  Chapo : <input class="form-control" type="text" name="chapo" value="<?= $post->getChapo(); ?>">
+                  Chapo : <input class="form-control" type="text" name="standfirst" value="<?= $post->getStandfirst(); ?>">
                 </div>
               </div>
               <div class="col-12">
                 <div class="form-group">
-                  Contenu : <input class="form-control" type="text" name="contenu" value="<?= $post->getContenu(); ?>">
+                  Contenu : <input class="form-control" type="text" name="content" value="<?= $post->getContent(); ?>">
                 </div>
               </div>             
               <div class="col-12">
                 <div class="form-group">
-                  Statut : <select class="form-control" name="statut">
-                <option value="Attente" <?php if($post->getStatut()=="Attente") {echo "selected";} ?>>En attente</option>
-                <option value="Publié"  <?php if($post->getStatut()=="Publié") {echo "selected";} ?>>Publié</option>                           
+                  Statut : <select class="form-control" name="published">
+                <option value="En attente" <?php if($post->getPublished()=="En attente") {echo "selected";} ?>>En attente</option>
+                <option value="Publié"  <?php if($post->getPublished()=="Publié") {echo "selected";} ?>>Publié</option>                           
                 </select>
                 </div>               
               </div>
               <div class="col-12">
                 <div class="form-group">
-                          <input class="form-control" type="hidden" name="utilisateurId" value="<?= $post->getUtilisateurId(); ?>">
+                          <input class="form-control" type="hidden" name="userId" value="<?= $post->getUserId(); ?>">
                 </div>
               </div>
             </div>
