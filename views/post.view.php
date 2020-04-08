8c9ec5f80bcc562ea5b457aecdc3a031b133a64d
glerique <gael.lerique@gmail.com>
2020-04-08 17:42:51 +0200

    Add comments in Post view

--- views/post.view.php
+++ views/post.view.php
@@ -35,59 +35,48 @@
         </div>
 
         <div class="comments-area">
-          <h4>Commentaires</h4>
+          <h4><?= count($comments) ?> Commentaires</h4>
+          <?php foreach ($comments as $values) { ?>
           <div class="comment-list">
             <div class="single-comment justify-content-between d-flex">
               <div class="user justify-content-between d-flex">
                 <div class="desc">
                   <p class="comment">
-                    Multiply sea night grass fourth day sea lesser rule open subdue female fill
-                    which them Blessed, give fill lesser bearing multiply sea night grass fourth
-                    day sea lesser
+                    <?= $values->getContent(); ?>
                   </p>
 
                   <div class="d-flex justify-content-between">
                     <div class="d-flex align-items-center">
                       <h5>
-                        <a href="#">Emilly Blunt</a>
+                       Ecrit Par <?= $values->getAuthor(); ?>
                       </h5>
-                      <p class="date">December 4, 2017 at 3:12 pm </p>
+                      <p class="date">le <?= $values->getCreationDate(); ?></p>
                     </div>
                   </div>
 
                 </div>
               </div>
             </div>
-          </div>
-
+          </div>              
+          <?php } ?>
         </div>
         <div class="comment-form">
           <h4>Laissez un commentaire</h4>
-          <form class="form-contact comment_form" action="#" id="commentForm">
+          <form class="form-contact comment_form" method ="post" action="index.php?controller=Comment&action=addComment" id="commentForm">
             <div class="row">
               <div class="col-12">
                 <div class="form-group">
-                  <textarea class="form-control w-100" name="comment" id="comment" cols="30" rows="9" placeholder="Write Comment"></textarea>
-                </div>
-              </div>
-              <div class="col-sm-6">
-                <div class="form-group">
-                  <input class="form-control" name="name" id="name" type="text" placeholder="Name">
-                </div>
-              </div>
-              <div class="col-sm-6">
-                <div class="form-group">
-                  <input class="form-control" name="email" id="email" type="email" placeholder="Email">
+                  <textarea class="form-control w-100" name="content"  cols="30" rows="9" placeholder="Ecrire un commentaire"></textarea>
                 </div>
               </div>
               <div class="col-12">
                 <div class="form-group">
-                  <input class="form-control" name="website" id="website" type="text" placeholder="Website">
+                    <input class="form-control" type="hidden" name="postId" value="<?= $post->getId(); ?>">
                 </div>
-              </div>
+              </div>              
             </div>
             <div class="form-group">
-              <button type="submit" class="button button-contactForm">Send Message</button>
+              <button type="submit" class="button button-contactForm">Envoyer Message</button>
             </div>
           </form>
         </div>
