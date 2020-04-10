d243f2cfb7b8dd964066542439ee7d38c56976d7
glerique <gael.lerique@gmail.com>
2020-04-10 14:38:10 +0200

    Create Contact Class

---
+++ models/Contact.php
@@ -0,0 +1,36 @@
+<?php
+namespace models;
+
+require 'lib/PHPMailer/src/PHPMailer.php';
+require 'lib/PHPMailer/src/SMTP.php';
+
+class Contact
+{
+public function sendEmail($lastname, $firstname, $email, $subject, $content)
+    {
+    $mail = new \PHPMailer\PHPMailer\PHPMailer(true);
+    $mail->isSMTP(); // Paramétrer le Mailer pour utiliser SMTP 
+    $mail->Host = 'smtp.gmail.com'; // Spécifier le serveur SMTP
+    $mail->SMTPAuth = true; // Activer authentication SMTP
+    $mail->Username = 'gael.maing59@gmail.com'; // Votre adresse email d'envoi
+    $mail->Password = 'ImaD1277'; // Le mot de passe de cette adresse email
+    $mail->SMTPSecure = 'ssl'; // Accepter SSL
+    $mail->Port = 465;
+    $mail->setFrom($email, $lastname.' '.$firstname); // Personnaliser l'envoyeur
+
+
+    //Recipients
+    
+    $mail->addAddress('gael.maing59@gmail.com', 'Message du Blog');     // Add a recipient
+    $mail->addReplyTo($email, 'Information');    
+    
+
+    // Content
+    
+    $mail->Subject = $subject;
+    $mail->Body = $content;
+
+    
+    $mail->send();
+    } 
+}
