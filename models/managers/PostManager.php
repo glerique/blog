0564c170f083a0fb11108e2a6a35ba004b74f59a
glerique <gael.lerique@gmail.com>
2020-04-14 12:06:39 +0200

    Create managers folder and refactor managers

---
+++ models/managers/PostManager.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace models\managers;
+
+
+
+class PostManager extends \models\Database
+{
+
+  private $db; // Instance de PDO
+
+  public function __construct()
+  {
+    $db = $this->dbConnect();
+    $this->setDb($db);
+  }
+
+  public function setDb(\PDO $db)
+  {
+    $this->db = $db;
+  }
+
+  public function count()
+  {
+    $query = $this->db->prepare('SELECT COUNT(*) FROM post');
+    $query->execute();
+    $count = $query->fetchColumn();
+    return $count;
+  }
+
+  public function add(\models\Post $post)
+  {
+    $query = $this->db->prepare('INSERT INTO post(title, standfirst, content, author, creationDate, published, userId)
+    VALUES(:title, :standfirst, :content, :author, :creationDate, :published, :userId)');
+    $query->bindValue(':title', $post->getTitle());
+    $query->bindValue(':standfirst', $post->getStandfirst());
+    $query->bindValue(':content', $post->getContent());
+    $query->bindValue(':author', $post->getAuthor());
+    $query->bindValue(':creationDate', $post->getCreationDate());
+    $query->bindValue(':published', $post->getPublished());
+    $query->bindValue(':userId', $post->getUserId());
+    $query->execute();
+  }
+
+  public function delete($id)
+  {
+    $query = $this->db->prepare('DELETE FROM post WHERE id = ' . $id);
+    
+    $query->execute();
+  }
+
+  public function get($id)
+  {
+    $id = (int) $id;
+    $query = $this->db->prepare('SELECT id, title, standfirst, content, author, date_format(creationDate,"%d/%m/%Y") AS creationDate, 
+                                    date_format(modificationDate,"%d/%m/%Y") AS modificationDate, published, userId FROM post WHERE id = ' . $id);
+    $query->execute();
+    if ($query->rowCount() != 1) {
+      return false; 
+    } else {
+
+      $donnees = $query->fetch(\PDO::FETCH_ASSOC);
+      return new \models\Post($donnees);
+    }
+  }
+
+  public function getList()
+  {
+
+    
+    $query = $this->db->prepare('SELECT id, title, standfirst, content, date_format(creationDate,"%d/%m/%Y") AS creationDate,
+                                   date_format(modificationDate,"%d/%m/%Y") AS modificationDate, published, userId FROM post');
+    $query->execute();
+    $query->setFetchMode(\PDO::FETCH_CLASS | \PDO::FETCH_PROPS_LATE,'\models\Post');
+   
+    $posts = $query->fetchAll();
+
+    
+    return $posts;
+  }  
+
+  public function getPublishedList()
+  {
+
+    $query = $this->db->prepare('SELECT id, title, standfirst, content, author, date_format(creationDate,"%d/%m/%Y") AS creationDate,
+                                     date_format(modificationDate,"%d/%m/%Y") AS modificationDate, published, userId FROM post WHERE published = "Publié" ORDER BY id DESC');
+    $query->execute();
+    $query->setFetchMode(\PDO::FETCH_CLASS | \PDO::FETCH_PROPS_LATE,'\models\Post');
+   
+    $posts = $query->fetchAll();
+    
+    return $posts;
+  }
+
+  public function update(\models\Post $post)
+  {
+    $query = $this->db->prepare('UPDATE post SET title = :title, standfirst = :standfirst, content = :content, author = :author,  
+                                                 modificationDate = :modificationDate, published = :published, userId = :userId  WHERE id = :id');
+    $query->bindValue(':title', $post->getTitle());
+    $query->bindValue(':standfirst', $post->getStandfirst());
+    $query->bindValue(':content', $post->getContent());
+    $query->bindValue(':author', $post->getAuthor());
+    $query->bindValue(':modificationDate', $post->getModificationDate());
+    $query->bindValue(':published', $post->getPublished());
+    $query->bindValue(':userId', $post->getUserId());
+    $query->bindValue(':id', $post->getId());
+    $query->execute();
+  }
+}
---
+++ models/managers/UserManager.php
@@ -0,0 +1,95 @@
+<?php
+namespace models\managers;
+class UserManager extends \models\Database
+{
+
+    private $db; // Instance de PDO
+
+    public function __construct()
+    {
+        $db = $this->dbConnect();
+        $this->setDb($db);
+    }
+
+    public function setDb(\PDO $db)
+    {
+        $this->db = $db;
+    }
+
+    public function count(){
+        $query = $this->db->prepare('SELECT COUNT(*) FROM user');
+        $query->execute();
+        $count = $query->fetchColumn();
+        return $count;
+      }
+    
+      public function add(\models\User $user){
+        $query = $this->db->prepare('INSERT INTO user(lastName, firstName, email, nickname, pswd, userRole)
+        VALUES(:nom, :prenom, :mail, :pseudo, :mdp, :droit)');
+        $query->bindValue(':nom', $user->getLastName());
+        $query->bindValue(':prenom', $user->getFirstName());
+        $query->bindValue(':mail', $user->getEmail());
+        $query->bindValue(':pseudo', $user->getNickname());
+        $query->bindValue(':mdp', $user->getPswd());
+        $query->bindValue(':droit', $user->getUserRole());        
+        $query->execute();
+      }
+    
+      public function delete($id){
+        $query= $this->db->prepare('DELETE FROM user WHERE id = '.$id);
+        $query->execute();
+      }
+    
+      public function get($id){
+        $id = (int) $id;
+        $query = $this->db->prepare('SELECT id, lastName, firstName, email, nickname, pswd, userRole FROM user WHERE id = '.$id);
+        $query->execute();
+        $data = $query->fetch(\PDO::FETCH_ASSOC);
+        return new \models\User($data);
+      }
+    
+        public function getNickname($id){
+        $id = (int) $id;
+        $query = $this->db->prepare('SELECT nickname FROM user WHERE id = '.$id);
+        $query->execute();
+        $data = $query->fetch(\PDO::FETCH_ASSOC);
+        //Permet d'obtenir le resultat en chiane de caratère et non en tableau 
+        return implode($data);
+      }
+      public function getList(){   
+        
+        
+        $query = $this->db->prepare('SELECT id, lastName, firstName, email, nickname, pswd, userRole FROM user');   
+        $query->execute();
+    
+        $query->setFetchMode(\PDO::FETCH_CLASS | \PDO::FETCH_PROPS_LATE,'\models\User');
+   
+        $users = $query->fetchAll();  
+        return $users;    
+        }
+    
+      public function update(\models\User $user){
+        $query = $this->db->prepare('UPDATE user SET lastName = :lastName, firstName = :firstName,  
+                                              nickname = :nickname,  userRole = :userRole WHERE id = :id');
+        $query->bindValue(':lastName', $user->getLastName());   
+        $query->bindValue(':firstName', $user->getFirstName());
+        $query->bindValue(':nickname', $user->getNickname());        
+        $query->bindValue(':userRole', $user->getUserRole());        
+        $query->bindValue(':id', $user->getId());
+        $query->execute();
+      }    
+
+    public function getByEmail($email)
+    {
+      $query = $this->db->prepare('SELECT * FROM user WHERE email = :email');
+      $query->execute([':email' => $email]);     
+      return $query->fetch(\PDO::FETCH_ASSOC);           
+    }
+
+    public function getByNickname($nickname)
+    {
+      $query = $this->db->prepare('SELECT * FROM user WHERE nickname = :nickname');
+      $query->execute([':nickname' => $nickname]);     
+      return $query->fetch(\PDO::FETCH_ASSOC);           
+    }
+}
