14d3c8dcf17e7594f7b211a9296273020880c352
glerique <gael.lerique@gmail.com>
2020-04-22 14:27:44 +0200

    Add token for CSRF exploit

--- views/post.view.php
+++ views/post.view.php
@@ -37,43 +37,42 @@
         <div class="comments-area">
           <h4><?= count($comments) ?> Commentaires</h4>
           <?php foreach ($comments as $values) { ?>
-          <div class="comment-list">
-            <div class="single-comment justify-content-between d-flex">
-              <div class="user justify-content-between d-flex">
-                <div class="desc">
-                  <p class="comment">
-                    <?= $values->getContent(); ?>
-                  </p>
+            <div class="comment-list">
+              <div class="single-comment justify-content-between d-flex">
+                <div class="user justify-content-between d-flex">
+                  <div class="desc">
+                    <p class="comment">
+                      <?= $values->getContent(); ?>
+                    </p>
 
-                  <div class="d-flex justify-content-between">
-                    <div class="d-flex align-items-center">
-                      <h5>
-                       Ecrit Par <?= $values->getAuthor(); ?>
-                      </h5>
-                      <p class="date">le <?= $values->getCreationDate(); ?></p>
+                    <div class="d-flex justify-content-between">
+                      <div class="d-flex align-items-center">
+                        <h5>
+                          Ecrit Par <?= $values->getAuthor(); ?>
+                        </h5>
+                        <p class="date">le <?= $values->getCreationDate(); ?></p>
+                      </div>
                     </div>
-                  </div>
 
+                  </div>
                 </div>
               </div>
             </div>
-          </div>              
           <?php } ?>
         </div>
         <div class="comment-form">
           <h4>Laissez un commentaire</h4>
-          <form class="form-contact comment_form" method ="post" action="index.php?controller=Comment&action=addComment" id="commentForm">
+          <form class="form-contact comment_form" method="post" action="index.php?controller=Comment&action=addComment" id="commentForm">
             <div class="row">
               <div class="col-12">
                 <div class="form-group">
-                  <textarea class="form-control w-100" name="content"  cols="30" rows="9" placeholder="Ecrire un commentaire"></textarea>
+                  <textarea class="form-control w-100" name="content" cols="30" rows="9" placeholder="Ecrire un commentaire"></textarea>
                 </div>
               </div>
-              <div class="col-12">
-                <div class="form-group">
-                    <input class="form-control" type="hidden" name="postId" value="<?= $post->getId(); ?>">
-                </div>
-              </div>              
+
+              <input class="form-control" type="hidden" name="postId" value="<?= $post->getId(); ?>">
+              <input class="form-control" type="hidden" name="token" value="<?= $_SESSION['token']; ?>">
+
             </div>
             <div class="form-group">
               <button type="submit" class="button button-contactForm">Envoyer Message</button>
