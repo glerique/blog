3be2eeb95b023a3f151abb1524ca34bef7779d79
glerique <gael.lerique@gmail.com>
2020-04-30 14:11:02 +0200

    Refactor Comment class

--- models/Comment.php
+++ models/Comment.php
@@ -2,96 +2,79 @@
 
 namespace models;
 
-class Comment {
+class Comment extends Model
+{
 
-    private $id;
     private $content;
     private $creationDate;
     private $validated;
-    private $postId; 
+    private $postId;
     private $userId;
     private $author;
 
-    public function hydrate(array $donnees){
-        foreach ($donnees as $key => $value){   
-            // On récupère le nom du setter correspondant à l'attribut.
-            $method = 'set'.ucfirst($key);
-            // Si le setter correspondant existe.
-            if (method_exists($this, $method)){
-            // On appelle le setter.
-            $this->$method($value);
-            }
-        }
-    }
+    //GETTERS   
 
-    // Important car sinon l'objet à sa création est vide.
-    public function __construct($valeurs = [])
+    function getContent()
     {
-      if (!empty($valeurs)) // Si on a spécifié des valeurs, alors on hydrate l'objet.
-      {
-        $this->hydrate($valeurs);
-      }
-    }
-
-    //GETTERS
-
-    function getId(){
-        return $this->id;
-    }
-
-    function getContent(){
         return $this->content;
-    }   
+    }
 
-    function getCreationDate(){
+    function getCreationDate()
+    {
         return $this->creationDate;
     }
-    
 
-    function getValidated(){
+
+    function getValidated()
+    {
         return $this->validated;
     }
 
-    function getPostId(){
+    function getPostId()
+    {
         return $this->postId;
     }
 
-    function getUserId(){
+    function getUserId()
+    {
         return $this->userId;
     }
 
-    function getAuthor(){
+    function getAuthor()
+    {
         return $this->author;
     }
 
 
     //SETTERS
 
-    function setId($id){
-        $this->id = $id;
-    }
-
-    function setContent($content){
+    function setContent($content)
+    {
         $this->content = $content;
     }
-    
-    function setCreationDate($creationDate){
+
+    function setCreationDate($creationDate)
+    {
         $this->creationDate = $creationDate;
-    }  
-    
-    function setValidated($validated){
+    }
+
+    function setValidated($validated)
+    {
         $this->validated = $validated;
     }
-    
-    function setPostId($postId){
-        $this->postId = $postId;
-    }  
 
-    function setUserId($userId){
+    function setPostId($postId)
+    {
+        $this->postId = $postId;
+    }
+
+    function setUserId($userId)
+    {
         $this->userId = $userId;
     }
-    
-    function setAuthor($author){
+
+    function setAuthor($author)
+    {
         $this->author = $author;
-    } 
+    }
 }
