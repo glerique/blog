9da4833a6c12f47e95cf210789be9152f0407f09
glerique <gael.lerique@gmail.com>
2020-04-30 14:12:16 +0200

    Refactor User class

--- models/User.php
+++ models/User.php
@@ -1,94 +1,79 @@
 <?php
-namespace Models;
-class User {
 
-    private $id;
+namespace Models;
+
+class User extends Model
+{
+
     private $lastName;
     private $firstName;
     private $email;
     private $nickname;
     private $pswd;
-    private $userRole; 
-
-    public function hydrate(array $donnees){
-        foreach ($donnees as $key => $value){
-        // On récupère le nom du setter correspondant à l'attribut.
-        $method = 'set'.ucfirst($key);
-        // Si le setter correspondant existe.
-        if (method_exists($this, $method)){
-        // On appelle le setter.
-        $this->$method($value);
-        }
-    }
-}
-
-// Important car sinon l'objet à sa création est vide.
-public function __construct($valeurs = [])
-{
-  if (!empty($valeurs)) // Si on a spécifié des valeurs, alors on hydrate l'objet.
-  {
-    $this->hydrate($valeurs);
-  }
-}
+    private $userRole;
 
     //GETTERS
 
-    function getId(){        
-	    return $this->id;
+    function getLastName()
+    {
+        return $this->lastName;
     }
 
-    function getLastName(){
-	    return $this->lastName;
-    }
-
-    function getFirstName(){
+    function getFirstName()
+    {
         return $this->firstName;
     }
 
-    function getEmail(){
+    function getEmail()
+    {
         return $this->email;
     }
 
-    function getNickname(){
+    function getNickname()
+    {
         return $this->nickname;
     }
 
-    function getPswd(){
+    function getPswd()
+    {
         return $this->pswd;
     }
 
-    function getUserRole(){
+    function getUserRole()
+    {
         return $this->userRole;
     }
-    
+
 
     //SETTERS
 
-    function setId($id){
-        $this->id = $id;
-    }
-
-    function setLastName($lastName){
+    function setLastName($lastName)
+    {
         $this->lastName = $lastName;
     }
-    
-    function setFirstName($firstName){
+
+    function setFirstName($firstName)
+    {
         $this->firstName = $firstName;
     }
 
-    function setEmail($email){
+    function setEmail($email)
+    {
         $this->email = $email;
     }
 
-    function setNickname($nickname){
+    function setNickname($nickname)
+    {
         $this->nickname = $nickname;
     }
 
-    function setPswd($pswd){
+    function setPswd($pswd)
+    {
         $this->pswd = $pswd;
     }
 
-    function setUserRole($userRole){
+    function setUserRole($userRole)
+    {
         $this->userRole = $userRole;
     }
 }
