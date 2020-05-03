be9c34355b35c61fe494e6e4a7f073211fa598f0
glerique <gael.lerique@gmail.com>
2020-05-03 15:21:32 +0200

    Add minor changes

--- views/validComment.view.php
+++ views/validComment.view.php
@@ -14,19 +14,21 @@
           <div class="row">
 
             <input class="form-control" type="hidden" name="id" value="<?= $comment->getId(); ?>">
+
+            <div class="col-12">
+              <div class="form-group">
+                Titre de l'article : <input class="form-control" type="text" name="title" value="<?= $comment->getTitle(); ?>" readonly>
+              </div>
+            </div>
+            <div class="col-12">
+              <div class="form-group">
+                Commentaire : <input class="form-control" type="text" name="title" value="<?= $comment->getContent(); ?>" readonly>
+              </div>
+            </div>
+
+            <input class="form-control" type="hidden" name="validated" value="2">
+            <input class="form-control" type="hidden" name="token" value="<?= $_SESSION['token']; ?>">
           </div>
-          <div class="col-12">
-            <div class="form-group">
-              Titre de l'article : <input class="form-control" type="text" name="title" value="<?= $comment->getTitle(); ?>" readonly>
-            </div>
-            </div>
-          <div class="col-12">
-            <div class="form-group">
-              Commentaire : <input class="form-control" type="text" name="title" value="<?= $comment->getContent(); ?>" readonly>
-            </div>
-          </div>
-          <input class="form-control" type="hidden" name="validated" value="2">
-          <input class="form-control" type="hidden" name="token" value="<?= $_SESSION['token']; ?>">
           <div class="form-group mt-3">
             <input type="submit" class="button button-contactForm" name="valider" value="valider">
             <input type="button" class="button button-contactForm" value="Retour" onClick="document.location.href = document.referrer" />
@@ -34,5 +36,5 @@
         </form>
       </div>
     </div>
-  </div>
+</div>
 </section>
